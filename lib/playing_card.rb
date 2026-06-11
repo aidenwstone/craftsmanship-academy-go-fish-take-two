0261d74b529dfd318e6ab1eb2061d0bef47bd907
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-06-11 16:30:13 -0400

    Add PlayingCard rank & suit validation

--- lib/playing_card.rb
+++ lib/playing_card.rb
@@ -4,7 +4,13 @@ class PlayingCard
   RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
   SUITS = %w[Spades Hearts Clubs Diamonds].freeze
 
+  class InvalidRank < StandardError; end
+  class InvalidSuit < StandardError; end
+
   def initialize(rank, suit)
+    raise InvalidRank unless RANKS.include?(rank)
+    raise InvalidSuit unless SUITS.include?(suit)
+
     @rank = rank
     @suit = suit
   end
--- spec/playing_card_spec.rb
+++ spec/playing_card_spec.rb
@@ -1,6 +1,22 @@
 require_relative '../lib/playing_card'
 
-describe PlayingCard do
+describe PlayingCard do # rubocop:disable Metrics/BlockLength
+  context 'when the given rank is not in valid rank list' do
+    it 'raises an invalid rank error' do
+      expect do
+        PlayingCard.new('15', 'Spades')
+      end.to raise_error PlayingCard::InvalidRank
+    end
+  end
+
+  context 'when the given suit is not in valid suit list' do
+    it 'raises an invalid suit error' do
+      expect do
+        PlayingCard.new('A', 'Invalid')
+      end.to raise_error PlayingCard::InvalidSuit
+    end
+  end
+
   describe '#to_s' do
     context 'with an A of Spades' do
       let(:card) { PlayingCard.new('A', 'Spades') }
