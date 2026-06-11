5de3a844e39213abf150088af2e61013569efb47
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-06-11 13:22:20 -0400

    Add GoFishPlayer#formatted_hand method

--- lib/go_fish_player.rb
+++ lib/go_fish_player.rb
@@ -8,4 +8,14 @@ class GoFishPlayer
   def add_cards(cards)
     cards.each { |card| hand << card }
   end
+
+  def formatted_hand
+    if hand.empty?
+      'You have no cards in your hand'
+    else
+      hand.reduce do |result, card|
+        "#{result}\n#{card}"
+      end
+    end
+  end
 end
--- spec/go_fish_player_spec.rb
+++ spec/go_fish_player_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/go_fish_player'
 require_relative '../lib/playing_card'
 
-describe GoFishPlayer do
+describe GoFishPlayer do # rubocop:disable Metrics/BlockLength
   let!(:player) { GoFishPlayer.new }
   let(:card1) { PlayingCard.new('A', 'Diamonds') }
   let(:card2) { PlayingCard.new('5', 'Hearts') }
@@ -21,6 +21,28 @@ describe GoFishPlayer do
         expect(player.hand.length).to eq 2
         expect(player.hand).to include card1
         expect(player.hand).to include card2
+      end
+    end
+  end
+
+  describe '#formatted_hand' do
+    context 'with no cards' do
+      it 'returns an empty hand message' do
+        empty_regex = /no cards/
+
+        result = player.formatted_hand
+        expect(result).to match empty_regex
+      end
+    end
+
+    context 'with cards' do
+      it 'returns the formatted hand' do
+        cards_regex = /A of Diamonds\s5 of Hearts/
+
+        player.add_cards([card1, card2])
+
+        result = player.formatted_hand
+        expect(result).to match cards_regex
       end
     end
   end
