f002ae2e2fe87c3be1297121759db7c91df66467
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-06-11 15:07:19 -0400

    Add rank & suit list constants to PlayingCard

--- lib/playing_card.rb
+++ lib/playing_card.rb
@@ -1,6 +1,9 @@
 class PlayingCard
   attr_reader :rank, :suit
 
+  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  SUITS = %w[Spades Hearts Clubs Diamonds].freeze
+
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
