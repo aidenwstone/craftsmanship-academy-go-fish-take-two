17ba7d0178a118506289dac06f2d42eaebee435c
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2026-06-11 18:36:36 -0400

    Add game variable to GameSession

--- lib/game_session.rb
+++ lib/game_session.rb
@@ -1,7 +1,15 @@
+require_relative 'go_fish_game'
+require_relative 'go_fish_player'
+
 class GameSession
-  attr_reader :clients
+  attr_reader :clients, :game
 
   def initialize(clients)
     @clients = clients
+    @game = GoFishGame.new(
+      clients.map do |client|
+        GoFishPlayer.new(client.id)
+      end
+    )
   end
 end
